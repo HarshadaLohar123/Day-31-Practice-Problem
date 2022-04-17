98ad58641231ee603051760af606e732c3407128
harshadalohar123 <harshalohar123@gmail.com>
2022-04-17 22:22:34 +0530

    [Harshada]Added:Welcome file.

---
+++ SQL-ER-Diagram/SQL-ER-Diagram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SQL_ER_Diagram
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to SQL ER Diagram....!");
+        }
+    }
+}
